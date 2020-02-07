6232f320163989a0ca1d180bdafd3924c75a0fb8
Harsha <Z1856779@students.niu.edu>
2020-02-06 19:21:35 -0600

    Hold button updated

--- Pig_Game/app.js
+++ Pig_Game/app.js
@@ -8,3 +8,59 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+//On click of roll dice random number from 1 to 6 has to be generated
+
+var randomNumber,activePlayer,dice;
+
+//random number generated
+randomNumber=0;
+
+activePlayer =0;
+//onclick on roll dice{
+document.querySelector(".btn-roll").addEventListener("click",function(){
+    dice = Math.floor((Math.random()*6)+1);
+    document.querySelector("#current-" +activePlayer).innerHTML=dice;
+   //Dice Image Change
+       if(dice==1){
+           document.querySelector(".dice").src="dice-1.png"
+       }
+       else if(dice==2){
+           document.querySelector(".dice").src="dice-2.png"
+       }
+    else if(dice==3){
+           document.querySelector(".dice").src="dice-3.png"
+       }
+    else if(dice==4){
+           document.querySelector(".dice").src="dice-4.png"
+       }
+    else if(dice==5){
+           document.querySelector(".dice").src="dice-5.png"
+       }
+     else if(dice==6){
+           document.querySelector(".dice").src="dice-6.png"
+       }
+    
+    
+});
+
+//onclick on Hold
+
+document.querySelector(".btn-hold").addEventListener("click",function(){
+    
+   
+    
+   
+});
+
+
+
+
+
+
+
+
+
+
+
+

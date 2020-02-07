5f4b9b15dc244fd83be4ca321dc33e6f3ddef8cf
Harsha <Z1856779@students.niu.edu>
2020-02-06 21:27:01 -0600

    Game_Updated

--- Pig_Game/app.js
+++ Pig_Game/app.js
@@ -11,18 +11,67 @@ GAME RULES:
 
 //On click of roll dice random number from 1 to 6 has to be generated
 
-var randomNumber,activePlayer,dice;
+var roundscore,activePlayer,dice,scores;
 
 //random number generated
-randomNumber=0;
-
+roundscore=0;
+scores=[0,0];
 activePlayer =0;
+resetAll();
+//reset the initial values
+function resetAll(){
+  document.querySelector('#current-0').textContent='0';
+document.querySelector('#current-1').textContent='0';
+document.querySelector('#score-0').textContent='0';
+document.querySelector('#score-1').textContent='0'; 
+   
+}
+
 //onclick on roll dice{
 document.querySelector(".btn-roll").addEventListener("click",function(){
     dice = Math.floor((Math.random()*6)+1);
-    document.querySelector("#current-" +activePlayer).innerHTML=dice;
-   //Dice Image Change
-       if(dice==1){
+   //scores update 
+    if(dice!==1){
+        //Adds the scores
+        roundscore+=dice;
+        document.querySelector("#current-" +activePlayer).innerHTML=roundscore;
+        }
+        else{          
+           nextPLayer();          
+         }   
+    //Dice Image Change
+       diceChanges();
+});
+
+//onclick on Hold
+
+document.querySelector(".btn-hold").addEventListener("click",function(){  
+  //1. Scores need to updated   
+    scores[activePlayer]+=roundscore;
+    document.querySelector("#score-" + activePlayer).innerHTML=scores[activePlayer];
+    if(scores[activePlayer]>=20){
+        document.querySelector("#name-" +activePlayer).innerHTML="Winner";
+        document.querySelector(".btn-roll").style.display="none";
+        document.getElementsByClassName(".btn-roll").disabled=true;
+        document.querySelector(".btn-hold").style.display="none";
+        document.querySelector(".dice").style.display="none";
+    }
+    else{
+       nextPLayer(); 
+    }    
+});
+
+function nextPLayer(){
+     activePlayer===0 ? activePlayer=1 : activePlayer=0;
+            roundscore=0;
+            document.querySelector('#current-0').textContent='0';
+            document.querySelector('#current-1').textContent='0';
+            document.querySelector('.player-0-panel').classList.toggle('active');
+            document.querySelector('.player-1-panel').classList.toggle('active');
+     
+}
+function diceChanges(){
+    if(dice==1){
            document.querySelector(".dice").src="dice-1.png"
        }
        else if(dice==2){
@@ -37,23 +86,19 @@ document.querySelector(".btn-roll").addEventListener("click",function(){
     else if(dice==5){
            document.querySelector(".dice").src="dice-5.png"
        }
-     else if(dice==6){
+     else{
            document.querySelector(".dice").src="dice-6.png"
        }
     
-    
+}
+//Reset to Newgame
+document.querySelector(".btn-new").addEventListener("click",function(){
+    resetAll();
+    document.querySelector(".btn-roll").style.display="block";
+        document.querySelector(".btn-hold").style.display="block";
+    document.querySelector(".dice").style.display="block";
 });
-
-//onclick on Hold
-
-document.querySelector(".btn-hold").addEventListener("click",function(){
     
-   
-    
-   
-});
-
-
 
 
 
